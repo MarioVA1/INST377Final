0a81233bf5dc0af1a83cee3af57c6f01d4814108
AmateurSenpai <112646850+MarioVA1@users.noreply.github.com>
2022-12-17 15:53:04 -0500

    got rid of arrayFromJson

--- script.js
+++ script.js
@@ -188,8 +188,6 @@ async function mainEvent() {
         This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
         It's at about line 27 - go have a look and see what we're retrieving and sending back.
        */
-  const results = await fetch('/api/foodServicePG');
-  const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
 
   const chartData = await getData();
   const shapedData = shapeDataForLineChart(chartData);
@@ -201,14 +199,14 @@ async function mainEvent() {
         Dot notation is preferred in JS unless you have a good reason to use brackets
         The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
       */
-  console.table(arrayFromJson.data);
+  console.table(chartData.data);
 
   // in your browser console, try expanding this object to see what fields are available to work with
   // for example: arrayFromJson.data[0].name, etc
-  console.log(arrayFromJson.data[0]);
+  console.log(chartData.data[0]);
 
   // this is called "string interpolation" and is how we build large text blocks with variables
-  console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
+  console.log(`${chartData[0].name} ${chartData[0].category}`);
 
   // This IF statement ensures we can't do anything if we don't have information yet
   if (chartData?.length > 0) {
